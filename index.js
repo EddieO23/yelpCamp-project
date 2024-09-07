f270fe2e9db2a6c445aaf9654569ff2aad54f319
eocampo52 <eocampo52@gmail.com>
2024-09-07 16:26:18 -0500

    CRUD. Added Delete functionality.

--- index.js
+++ index.js
@@ -48,11 +48,19 @@ app.get("/campgrounds/:id/edit", async (req, res) => {
   res.render("campgrounds/edit", { campground });
 });
 
-app.put('/campgrounds/:id', async(req, res) => {
-  const {id} = req.params
-  const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
   res.redirect(`/campgrounds/${campground._id}`);
-})
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id)
+  res.redirect('/campgrounds')
+});
 
 app.listen(3000, () => {
   console.log("Serving on port 3000");
