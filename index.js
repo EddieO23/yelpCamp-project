e3b042c95169ca4e5de2874190c0713bc7e37e78
eocampo52 <eocampo52@gmail.com>
2024-09-07 14:23:43 -0500

    you can now see individual places in show campgrounds

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const express = require('express')
-const path = require('path')
-const mongoose = require('mongoose')
-const Campground = require('./models/campground')
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, "connection error:"))
+db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-  console.log('Database connected')
-})
+  console.log("Database connected");
+});
 
-const app = express()
+const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.get("/campgrounds", async (require, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
 
-app.get('/campgrounds' , async (require, res) => {
-  const campgrounds = await Campground.find({}) 
-  res.render('campgrounds/index', {campgrounds})
-})
-
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
 
 app.listen(3000, () => {
-  console.log('Serving on port 3000')
-})
+  console.log("Serving on port 3000");
+});
