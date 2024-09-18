61df2b754d346f0f9bee3f7aa31038065b9fcd95
Eddie Ocampo <eddie@Eddies-MacBook-Air.local>
2024-09-17 20:34:02 -0500

    added error handling basic for new campground

--- index.js
+++ index.js
@@ -33,10 +33,14 @@ app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
 
-app.post("/campgrounds", async (req, res) => {
+app.post("/campgrounds", async (req, res, next) => {
+  try {
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
+  } catch(e) {
+    next(e)
+  }
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
@@ -62,6 +66,10 @@ app.delete("/campgrounds/:id", async (req, res) => {
   await Campground.findByIdAndDelete(id)
   res.redirect('/campgrounds')
 });
+
+app.use((err, req, res, next) => {
+  res.send('oh boy, something went wrong!')
+})
 
 app.listen(3000, () => {
   console.log("Serving on port 3000");
