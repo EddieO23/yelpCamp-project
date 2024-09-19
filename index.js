f9f0a3656fcf3462f8cce73b660f33b8e499cef9
Eddie <eocampo52@gmail.com>
2024-09-19 12:28:02 -0500

    added Joi validaiton for every input required. Error handling in Express

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const Joi = require("joi");
+const { campgroundSchema } = require("./schemas.js");
 const catchAsync = require("./utils/catchAsync");
 const expressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
@@ -26,6 +26,16 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const message = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(message, 400);
+  } else {
+    next();
+  }
+};
+
 //routes
 
 app.get(
@@ -42,24 +52,8 @@ app.get("/campgrounds/new", (req, res) => {
 
 app.post(
   "/campgrounds",
+  validateCampground,
   catchAsync(async (req, res, next) => {
-    // if (!req.body.campground)
-    //   throw new ExpressError("Invalid Campground Data", 404);
-    const campgroundSchema = Joi.object({
-      campground: Joi.object({
-        title: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        image: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required()
-      }).required(),
-    });
-    const {error} = campgroundSchema.validate(req.body)
-    if(error) {
-      const message = error.details.map(el => el.message).join(',')
-      throw new ExpressError(message, 400)
-    }
-    console.log(result)
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -81,9 +75,10 @@ app.get(
     res.render("campgrounds/edit", { campground });
   })
 );
-
+// Update campground route
 app.put(
   "/campgrounds/:id",
+  validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {
