9c97e37e0728da6d5681c6f4d0eb7aa33ce3a86d
Eddie <eocampo52@gmail.com>
2024-10-17 13:36:21 -0500

    added user routes to app. added user registration form to app

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const session = require('express-session');
-const flash = require('connect-flash')
+const session = require("express-session");
+const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
-
-
-const campgrounds = require("./routes/campgrounds.js");
-const reviews = require("./routes/reviews.js");
+const userRoutes = require('./routes/users.js')
+const campgroundRoutes = require("./routes/campgrounds.js");
+const reviewRoutes = require("./routes/reviews.js");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
@@ -28,34 +30,41 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 const sessionConfig = {
-  secret: 'thisshouldbeabettersecret!',
+  secret: "thisshouldbeabettersecret!",
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge:  1000 * 60 * 60 * 24 * 7
-  }
-}
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
 
-app.use(session(sessionConfig))
-app.use(flash())
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
   next();
-})
+});
 
-app.use("/campgrounds", campgrounds);
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/', userRoutes)
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
-
-app.get('/', (req, res) => {
-  res.render('home')
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 // 404 Route
