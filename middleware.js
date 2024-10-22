c598ddd62ebaf1dda622348962fa1d47a4bfbeed
Eddie <eocampo52@gmail.com>
2024-10-22 17:13:33 -0500

    added dynamic navbar for user. if user logged in it will show logout link. If user not logged in, it will show register and login links

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
+  console.log("Req.user...", req.user)
   if (!req.isAuthenticated()) {
     req.flash("error", "You must be signed in!");
     return res.redirect("/login");
