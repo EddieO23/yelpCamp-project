5d7a3b0b7c06887399c62f35506d7ed33f9f6016
Eddie <eocampo52@gmail.com>
2024-10-22 17:21:13 -0500

    added consitent login for new user across app

--- routes/users.js
+++ routes/users.js
@@ -10,14 +10,16 @@ router.get("/register", (req, res) => {
 
 router.post(
   "/register",
-  catchAsync(async (req, res) => {
+  catchAsync(async (req, res, next) => {
     try {
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
-      console.log(registeredUser);
-      req.flash("success", "Welcome to YelpCamp!");
-      res.redirect("/campgrounds");
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash("success", "Welcome to YelpCamp!");
+        res.redirect("/campgrounds");
+      });
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("register");
@@ -29,21 +31,26 @@ router.get("/login", (req, res) => {
   res.render("users/login");
 });
 
-router.post('/login',  passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
-  req.flash('success', 'welcome back!')
-  res.redirect('/campgrounds')
-})
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.flash("success", "welcome back!");
+    res.redirect("/campgrounds");
+  }
+);
 
-router.get('/logout', (req, res, next) => {
+router.get("/logout", (req, res, next) => {
   req.logout(function (err) {
-      if (err) {
-          return next(err);
-      }
-      req.flash('success', 'Goodbye!');
-      res.redirect('/campgrounds');
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "Goodbye!");
+    res.redirect("/campgrounds");
   });
-}); 
-
-
+});
 
 module.exports = router;
