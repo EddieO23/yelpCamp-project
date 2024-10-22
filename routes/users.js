d7af0a706f290c32ea3f17497a2c8e1e8d557366
Eddie <eocampo52@gmail.com>
2024-10-22 17:03:22 -0500

    added logout functionality to app

--- routes/users.js
+++ routes/users.js
@@ -34,4 +34,16 @@ router.post('/login',  passport.authenticate('local', {failureFlash: true, failu
   res.redirect('/campgrounds')
 })
 
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+      if (err) {
+          return next(err);
+      }
+      req.flash('success', 'Goodbye!');
+      res.redirect('/campgrounds');
+  });
+}); 
+
+
+
 module.exports = router;
