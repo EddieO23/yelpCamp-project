8d439bb47bcca96a2447a73e6ce89d384023d2d5
Eddie <eocampo52@gmail.com>
2024-10-22 19:50:14 -0500

    added functionality where user wherever they login from they will succesfully go back to the page where they left from

--- middleware.js
+++ middleware.js
@@ -1,9 +1,15 @@
 module.exports.isLoggedIn = (req, res, next) => {
-  console.log("Req.user...", req.user)
   if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl
     req.flash("error", "You must be signed in!");
     return res.redirect("/login");
   }
   next()
 };
 
+module.exports.storeReturnTo = (req, res, next) => {
+  if(req.session.returnTo) {
+    res.locals.returnTo = req.session.returnTo
+  }
+  next()
+}
