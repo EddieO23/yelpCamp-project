5df2099003013a61fc62a16d3a68b8fde33bd5f8
Eddie <eocampo52@gmail.com>
2024-10-24 14:09:21 -0500

    added the ability to not view form to leave review if user is not logged in. as well added author to reviewSchema

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const router = express.Router({mergeParams: true});
-const {validateReview} = require('../middleware')
+const router = express.Router({ mergeParams: true });
+const { validateReview, isLoggedIn } = require("../middleware");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 
@@ -8,17 +8,18 @@ const Review = require("../models/review");
 const ExpressError = require("../utils/ExpressError");
 const catchAsync = require("../utils/catchAsync");
 
-
 router.post(
   "/",
+  isLoggedIn,
   validateReview,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id
     campground.reviews.push(review);
     await review.save();
     await campground.save();
-    req.flash('success', 'Created new review')
+    req.flash("success", "Created new review");
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
@@ -50,7 +51,7 @@ router.delete(
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'You deleted a review')
+    req.flash("success", "You deleted a review");
     res.redirect(`/campgrounds/${id}`);
   })
 );
