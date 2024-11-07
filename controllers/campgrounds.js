cf0dd68d740039b421c02c6f0917a47b5be3e949
Eddie <eocampo52@gmail.com>
2024-11-07 14:33:25 -0600

    added thumbnail preview to images in the edit page. added the ability to delete images from mongoose and cloudinary

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require("../models/campground");
+const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -17,7 +18,7 @@ module.exports.createCampground = async (req, res, next) => {
   }));
   campground.author = req.user._id;
   await campground.save();
-  console.log(campground);
+  // console.log(campground);
   req.flash("success", "Successfully made a new campground!");
   res.redirect(`/campgrounds/${campground._id}`);
 };
@@ -50,13 +51,22 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
   const { id } = req.params;
-  console.log(req.body)
+  // console.log(req.body);
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
   const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
   campground.images.push(...imgs);
-  await campground.save()
+  await campground.save();
+  if (req.body.deleteImages) {
+    for( let filename of req.body.deleteImages ) {
+      await cloudinary.uploader.destroy(filename)
+    }
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
+    // console.log(campground);
+  }
   req.flash("success", "Successfully updated campground!");
   res.redirect(`/campgrounds/${campground._id}`);
 };
--- models/campground.js
+++ models/campground.js
@@ -2,9 +2,19 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+// https://res.cloudinary.com/dbsmctgow/image/upload/c_crop,g_auto,h_800,w_800/YelpCamp/sctmvb2jb0dho48eyajz.jpg
+
+const ImageSchema = new Schema({
+  url: String,
+  filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+  return this.url.replace('/upload', '/upload/c_crop,g_auto,h_100,w_100');
+});
 const CampgroundSchema = new Schema({
   title: String,
-  images: [{ url: String, filename: String }],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
